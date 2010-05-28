29c0e780905f6a2920756ef77bef4e283166a485
jrun <jeremy.burks@gmail.com>
2010-05-28 08:13:33 -0500

    extend self, everyone is doing it...

--- lib/gemslicer.rb
+++ lib/gemslicer.rb
@@ -3,39 +3,40 @@ require 'gemslicer/vault'
 require 'gemslicer/slicer'
 
 module Gemslicer
-  def self.server_root=(root)
+  extend self
+  
+  def server_root=(root)
     @server_root = File.expand_path(root)      
   end
   
-  def self.server_root
+  def server_root
     @server_root ||= File.expand_path File.join(File.dirname(__FILE__), '..', 'server')
   end
 
-  def self.reset_server_root
+  def reset_server_root
     @server_root = nil
   end
 
-  def self.server_path(*more)
+  def server_path(*more)
     File.join(self.server_root, *more)
   end
     
-  def self.proxy_server_path(*more)
+  def proxy_server_path(*more)
     server_path('proxy_tmp', *more)
   end
 
-  def self.indexer
+  def indexer
     Slicer.indexer
   end
     
   # Generating the index is an easy way to make sure required
   # directories exists  
-  def self.ensure_server_paths_exist
+  def ensure_server_paths_exist
     Gemslicer::Slicer.new_indexer(server_path).generate_index    
     FileUtils.mkdir_p File.join(server_path, 'gems')
   end
 
-  def self.ensure_proxy_server_paths_exist   
+  def ensure_proxy_server_paths_exist   
     Gemslicer::Slicer.new_indexer(proxy_server_path).generate_index
-  end
-  
+  end  
 end
